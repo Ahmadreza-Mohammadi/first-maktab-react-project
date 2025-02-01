d31a0dd929dfffedab1605b78281f6ba770e316e
noway777 <ahmadrezamohammadi777@gmail.com>
2025-02-01 15:12:27 +0330

    feat: offers section created

--- src/components/home/header/headerComponent/Header.jsx
+++ src/components/home/header/headerComponent/Header.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./header.css";
 import HeaderCard from "../headerCard/HeaderCard";
 import Subscription from "../subscription/Subscription";
+import Offers from "../offers/Offers";
 
 function Header() {
   return (
     <div className="header-container">
-      <Subscription />
+      <div className="header-left-side">
+        <Subscription />
+        <Offers />
+      </div>
       <HeaderCard />
     </div>
   );
---
+++ src/components/home/header/offers/Offers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Offers() {
+  return (
+    <div className="offers-container">
+      <h1 className="offers-title">Join Our Customer Club & Enjoy 30% Off Your First Order!</h1>
+      <p className="offers-description">
+        Become a member of our exclusive Customer Club and savor the taste of
+        Italy with a special 30% discount on your first order! Don’t miss out on
+        this delicious deal – sign up today and treat yourself to authentic
+        Italian pizza at an unbeatable price.
+      </p>
+      <button className="offers-button">Sign Up Now</button>
+    </div>
+  );
+}
+
+export default Offers;
