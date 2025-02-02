98ebcba1cda4d6f64d18c4567b633d10aadd0195
noway777 <ahmadrezamohammadi777@gmail.com>
2025-02-02 14:19:12 +0330

    fix: offers section styles fixed

--- src/components/home/home-component/HomePage.jsx
+++ src/components/home/home-component/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../navigation/navbar/Navbar";
 import Header from "../header/header-component/Header";
+import SocialMedia from "../social-media/SocialMedia";
 
 function HomePage() {
   return (
     <>
       <Navbar />
       <Header />
+      <SocialMedia />
     </>
   );
 }
