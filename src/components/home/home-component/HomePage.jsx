046a0dfa37aa252d5847eb334a4a120dc9eb603d
noway777 <ahmadrezamohammadi777@gmail.com>
2025-02-04 13:33:01 +0330

    fix: some styles in social media links has been changed

--- src/components/home/home-component/HomePage.jsx
+++ src/components/home/home-component/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../navigation/navbar/Navbar";
 import Header from "../header/header-component/Header";
 import SocialMedia from "../social-media/social-media-component/SocialMedia";
-import Footer from "../../footer/Footer";
+import Footer from "../footer/Footer";
 
 function HomePage() {
   return (
